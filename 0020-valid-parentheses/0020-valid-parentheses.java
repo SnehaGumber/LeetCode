39604516857d297f6a478e56cc4aa9406cc6398e
Sneha <sneha.gumber01@gmail.com>
2026-08-12 12:48:11 +0530

    Time: 3 ms (86.05%), Space: 43 MB (93.49%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n%2!=0) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<n; i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
+            else{
+                if(st.isEmpty()) return false;
+                else if((ch==')' && st.peek()=='(') || (ch==']' && st.peek()=='[') || (ch=='}' && st.peek()=='{')) st.pop();
+                else return false;
+            }
+        }
+        return st.isEmpty()? true : false;
+    }
+}
