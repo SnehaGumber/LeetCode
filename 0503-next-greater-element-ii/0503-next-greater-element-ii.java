88c677ecbd3dce82f2752f963172816eea6ebd4b
Sneha <sneha.gumber01@gmail.com>
2026-08-14 11:29:40 +0530

    Time: 15 ms (61.31%), Space: 48.6 MB (35.48%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,13 +4,13 @@ class Solution {
         int n = nums.length;
         int[] nge = new int[n];
         for(int i=2*n-1; i>=0; i--){
-            st.push(nums[i%n]);
-        }
-        for(int i=n-1; i>=0; i--){
-            while(!st.isEmpty() && st.peek()<=nums[i]) st.pop();
-            if(!st.isEmpty() && st.peek() > nums[i]) nge[i] = st.peek();
-            if(st.isEmpty()) nge[i] = -1;
-            st.push(nums[i]);
+            int current = nums[i%n];
+            while(!st.isEmpty() && st.peek()<=current)st.pop();
+            if(i<n){
+                if(st.isEmpty()) nge[i] = -1;
+                else nge[i] = st.peek(); 
+            }
+            st.push(current);
         }
         return nge;
     }
