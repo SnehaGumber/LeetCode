d6758464ab4a022c106afd9e0510dbd1ad39c016
Sneha <sneha.gumber01@gmail.com>
2026-09-08 12:04:57 +0530

    Time: 0 ms (100%), Space: 43.6 MB (27.12%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int f(int i, int j, int[][] dp, int m, int n, int[][] arr){
+        if(arr[i][j] == 1) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(i==m-1 && j==n-1) return 1;
+
+        else if(i == m-1 && arr[i][j+1] != 1){
+            // move right
+            dp[i][j] = f(i, j+1, dp, m, n , arr);
+        }
+
+        else if(j == n-1 && i!=m-1 && arr[i+1][j]!=1){
+            // move down
+            dp[i][j] = f(i+1, j, dp, m, n, arr);
+        }
+
+        else{
+            if(i < m-1 && j < n-1 && arr[i+1][j] != 1 && arr[i][j+1] != 1){
+                int down = f(i+1, j, dp, m, n, arr);
+                int right = f(i, j+1, dp, m, n, arr);
+                dp[i][j]= down+right;
+            }
+            else if(i<m-1 && arr[i+1][j] != 1){
+                dp[i][j] = f(i+1, j, dp, m, n, arr);
+            }  
+            else if(j<n-1 && arr[i][j+1] != 1){
+                dp[i][j] = f(i, j+1, dp, m, n, arr);
+            }
+            else dp[i][j] = 0;
+        }
+        return dp[i][j];
+    }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++) Arrays.fill(dp[i], -1);
+        return f(0, 0, dp, m, n, obstacleGrid);
+    }
+}
