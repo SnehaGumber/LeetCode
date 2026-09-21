7fd9dea6ee2a5078ffa4cebdeb301cdb46cfec47
Sneha <sneha.gumber01@gmail.com>
2026-09-21 10:58:40 +0530

    Time: 22 ms (81.31%), Space: 54.3 MB (9.45%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,33 +1,88 @@
-class Solution {
-    public int SCSS(int i, int j, int[][] dp, String str1, String str2){
+// class Solution {
+//     public int SCSS(int i, int j, int[][] dp, String str1, String str2){
+//         int n1 = str1.length();
+//         int n2 = str2.length();
+//         if(i == n1) dp[i][j] = n2-j;
+//         if(j == n2) dp[i][j] = n1-i;
+//         if(dp[i][j] != -1) return dp[i][j];
+//         if(str1.charAt(i) == str2.charAt(j)){
+//             dp[i][j] = 1+ SCSS(i+1, j+1, dp, str1, str2);
+//         }
+//         else{
+//             int first = 1 + SCSS(i+1, j, dp, str1, str2);
+//             int second = 1 + SCSS(i, j+1, dp, str1, str2);
+//             dp[i][j] = Math.min(first, second);
+//         }
+//         return dp[i][j];
+//     }
+//     public String shortestCommonSupersequence(String str1, String str2) {
+//         int[][] dp = new int[str1.length()+1][str2.length()+1];
+//         for(int i=0; i<str1.length(); i++) Arrays.fill(dp[i], -1);
+//         SCSS(0, 0, dp, str1, str2);
+//         StringBuilder sb = new StringBuilder();
+//         int i=0;
+//         int j=0;
+//         while(i<str1.length() && j<str2.length()){
+//             if(str1.charAt(i) == str2.charAt(j)){
+//                 sb.append(str1.charAt(i));
+//                 i++;
+//                 j++;
+//             }else{
+//                 if(dp[i+1][j] < dp[i][j+1]){
+//                     sb.append(str1.charAt(i));
+//                     i++;
+//                 }else{
+//                     sb.append(str2.charAt(j));
+//                     j++;
+//                 }
+//             }
+//         }
+//         while(i<str1.length()){
+//             sb.append(str1.charAt(i));
+//             i++;
+//         }
+//         while(j < str2.length()){
+//             sb.append(str2.charAt(j));
+//             j++;
+//         }
+//         return sb.toString();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+class Solution{
+    public String shortestCommonSupersequence(String str1, String str2){
         int n1 = str1.length();
         int n2 = str2.length();
-        if(i == n1) dp[i][j] = n2-j;
-        if(j == n2) dp[i][j] = n1-i;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(str1.charAt(i) == str2.charAt(j)){
-            dp[i][j] = 1+ SCSS(i+1, j+1, dp, str1, str2);
+        int[][] dp = new int[n1+1][n2+1];
+        for(int j=0; j<=n2; j++) dp[n1][j] = n2-j;
+        for(int i=0; i<=n1; i++) dp[i][n2] = n1-i;
+        for(int i=n1-1; i>=0; i--){
+            for(int j=n2-1; j>=0; j--){
+                if(str1.charAt(i) == str2.charAt(j)) dp[i][j] = 1 + dp[i+1][j+1];
+                else{
+                    int first = 1 + dp[i+1][j];
+                    int second = 1 + dp[i][j+1];
+                    dp[i][j] = Math.min(first, second);
+                }
+            }
         }
-        else{
-            int first = 1 + SCSS(i+1, j, dp, str1, str2);
-            int second = 1 + SCSS(i, j+1, dp, str1, str2);
-            dp[i][j] = Math.min(first, second);
-        }
-        return dp[i][j];
-    }
-    public String shortestCommonSupersequence(String str1, String str2) {
-        int[][] dp = new int[str1.length()+1][str2.length()+1];
-        for(int i=0; i<str1.length(); i++) Arrays.fill(dp[i], -1);
-        SCSS(0, 0, dp, str1, str2);
         StringBuilder sb = new StringBuilder();
         int i=0;
-        int j=0;
-        while(i<str1.length() && j<str2.length()){
+        int j = 0;
+        while(i<n1 && j<n2){
             if(str1.charAt(i) == str2.charAt(j)){
                 sb.append(str1.charAt(i));
-                i++;
-                j++;
-            }else{
+                i++; j++;
+            }
+            else{
                 if(dp[i+1][j] < dp[i][j+1]){
                     sb.append(str1.charAt(i));
                     i++;
@@ -37,11 +92,11 @@ class Solution {
                 }
             }
         }
-        while(i<str1.length()){
+        while(i < n1){
             sb.append(str1.charAt(i));
             i++;
         }
-        while(j < str2.length()){
+        while(j < n2){
             sb.append(str2.charAt(j));
             j++;
         }
