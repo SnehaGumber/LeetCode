013e631bcef21f93990dd8d252c861be0f52ab48
Sneha <sneha.gumber01@gmail.com>
2026-09-24 10:48:35 +0530

    Time: 1 ms (69.03%), Space: 43.4 MB (77.95%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,26 +1,48 @@
-class Solution {
-    public int solve(int i, int holding, int[][] dp, int[] arr){
-        if(i >= arr.length) return 0;
-        if(dp[i][holding] != -1) return dp[i][holding];
-        if(holding == 0){
-            // buy today
-            int buyToday = -arr[i] + solve(i+1, 1, dp, arr);
-            // buy later
-            int buyLater = solve(i+1, holding, dp, arr);
-            dp[i][holding] = Math.max(buyToday, buyLater);
-        }else{
-            // sell today (cannot buy tommorow)
-            int sellToday = arr[i] + solve(i+2, 0, dp, arr);
-            // sell later
-            int sellLater = solve(i+1, holding, dp, arr);
-            dp[i][holding] = Math.max(sellToday, sellLater);
-        }
-        return dp[i][holding];
-    }
-    public int maxProfit(int[] prices) {
+// class Solution {
+//     public int solve(int i, int holding, int[][] dp, int[] arr){
+//         if(i >= arr.length) return 0;
+//         if(dp[i][holding] != -1) return dp[i][holding];
+//         if(holding == 0){
+//             // buy today
+//             int buyToday = -arr[i] + solve(i+1, 1, dp, arr);
+//             // buy later
+//             int buyLater = solve(i+1, holding, dp, arr);
+//             dp[i][holding] = Math.max(buyToday, buyLater);
+//         }else{
+//             // sell today (cannot buy tommorow)
+//             int sellToday = arr[i] + solve(i+2, 0, dp, arr);
+//             // sell later
+//             int sellLater = solve(i+1, holding, dp, arr);
+//             dp[i][holding] = Math.max(sellToday, sellLater);
+//         }
+//         return dp[i][holding];
+//     }
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int[][] dp = new int[n+1][2];
+//         for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
+//         return solve(0, 0, dp, prices);
+//     }
+// }
+
+
+class Solution{
+    public int maxProfit(int[] prices){
         int n = prices.length;
-        int[][] dp = new int[n+1][2];
-        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
-        return solve(0, 0, dp, prices);
+        int[][] dp = new int[n+2][2];
+        for(int i=n-1; i>=0; i--){
+            for(int holding=0; holding<=1; holding++){
+                if(holding==0){
+                    int buyToday = -prices[i] + dp[i+1][1];
+                    int buyLater = dp[i+1][holding];
+                    dp[i][holding] = Math.max(buyToday, buyLater);
+                }else{
+                    int sellToday = prices[i] + dp[i+2][0];
+                    int sellLater = dp[i+1][holding];
+                    dp[i][holding] = Math.max(sellToday, sellLater);
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
